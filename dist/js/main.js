ffc328ca7bcf32d6463529c64466a23714da531e
Mike K <mikhail.karkachov@gmail.com>
2018-02-28 03:09:23 +0200

    compress images, babel

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,40 +1,41 @@
 'use strict';
 
 // 1. adding class to <li> with dropdow in #nav
-let nav = document.querySelectorAll('.nav-holder');
-if (nav.length > 0){
-    for (let i=0; i < nav.length; i++){
-        let drops = nav[i].querySelectorAll('.drop');
-        for (let j=0; j < drops.length; j++){
+
+var nav = document.querySelectorAll('.nav-holder');
+if (nav.length > 0) {
+    for (var i = 0; i < nav.length; i++) {
+        var drops = nav[i].querySelectorAll('.drop');
+        for (var j = 0; j < drops.length; j++) {
             drops[j].parentNode.classList.add('js-dropdown');
         }
     }
 }
 
 // 2. slider in testimonials block
-let quotesSlider = document.getElementById('quotes-slider');
-if (quotesSlider){
+var quotesSlider = document.getElementById('quotes-slider');
+if (quotesSlider) {
     tns({
-      container: quotesSlider,
-      items: 1,
-      mode: "gallery",
-      speed: 700,
-      axis: 'horizontal',
-      lazyload: true
+        container: quotesSlider,
+        items: 1,
+        mode: "gallery",
+        speed: 700,
+        axis: 'horizontal',
+        lazyload: true
     });
 }
 
 // 3. adding animations to elements in current viewport
-let sections = document.querySelectorAll('.js-flyin');
-if (sections.length > 0){
+var sections = document.querySelectorAll('.js-flyin');
+if (sections.length > 0) {
     inView();
-    window.onscroll = () =>{
+    window.onscroll = function () {
         inView();
-    }
+    };
 }
-function inView(){
+function inView() {
     if (window.innerWidth > 768) {
-        sections.forEach((section) => {
+        sections.forEach(function (section) {
             if (inViewport(section, { offset: -300 })) {
                 section.classList.add('js-inview');
             }
@@ -43,15 +44,15 @@ function inView(){
 }
 
 // 4. show navigation dropdown on mobile devices by click
-let navHolder = document.querySelector('.nav-holder');
-let opener = navHolder.querySelector('.opener');
-if (opener){
-    opener.onclick = () => {
+var navHolder = document.querySelector('.nav-holder');
+var opener = navHolder.querySelector('.opener');
+if (opener) {
+    opener.onclick = function () {
         navHolder.classList.toggle('opened');
-    }
-    window.onresize = () => {
-        if (navHolder.classList.contains('opened')){
+    };
+    window.onresize = function () {
+        if (navHolder.classList.contains('opened')) {
             navHolder.classList.remove('opened');
         }
-    }
+    };
 }
