2edca800ca9417980333f33722915249deba296f
Mike K <mikhail.karkachov@gmail.com>
2018-03-06 22:31:02 +0200

    form validation, post completed block

--- app/js/main.js
+++ app/js/main.js
@@ -55,3 +55,46 @@ if (opener){
         }
     }
 }
+
+// 4. Contact Form Falidation
+let contactForm = document.querySelector('.contact-form');
+contactForm.querySelector('.btn').onclick = () => {
+    contactFormValidation();
+}
+function contactFormValidation(){
+    let fields = contactForm.querySelectorAll('.required');
+    let validForm = true;
+    fields.forEach((field) => {
+        if (field.classList.contains('js-error')){
+            field.classList.remove('js-error');
+            field.parentNode.removeChild(field.parentNode.querySelector('.error-text'));
+        }
+        if (field.value.length < 3){
+            let errorText = field.parentNode.querySelector('label').textContent + ' is required!';
+            validationError(field, errorText);
+            validForm = false;
+        }
+        else{
+            if (field.getAttribute('type') === 'email'){
+                let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if (!regex.test(field.value.toLowerCase())){
+                    let errorText = field.parentNode.querySelector('label').textContent + ' is not valid!';
+                    validationError(field, errorText);
+                    validForm = false;
+                }
+            }
+        }
+    });
+    if (validForm){
+        // contactForm.submit();
+        contactForm.classList.add('js-validform')
+    }
+}
+
+function validationError(field, errorText){
+    field.classList.add('js-error');
+    let p = document.createElement("p");
+    p.appendChild(document.createTextNode(errorText));
+    p.classList.add('js-error-text');
+    field.parentNode.appendChild(p);
+}
--- dist/js/main.js
+++ dist/js/main.js
@@ -56,3 +56,45 @@ if (opener) {
         }
     };
 }
+
+// 4. Contact Form Falidation
+var contactForm = document.querySelector('.contact-form');
+contactForm.querySelector('.btn').onclick = function () {
+    contactFormValidation();
+};
+function contactFormValidation() {
+    var fields = contactForm.querySelectorAll('.required');
+    var validForm = true;
+    fields.forEach(function (field) {
+        if (field.classList.contains('js-error')) {
+            field.classList.remove('js-error');
+            field.parentNode.removeChild(field.parentNode.querySelector('.error-text'));
+        }
+        if (field.value.length < 3) {
+            var errorText = field.parentNode.querySelector('label').textContent + ' is required!';
+            validationError(field, errorText);
+            validForm = false;
+        } else {
+            if (field.getAttribute('type') === 'email') {
+                var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if (!regex.test(field.value.toLowerCase())) {
+                    var _errorText = field.parentNode.querySelector('label').textContent + ' is not valid!';
+                    validationError(field, _errorText);
+                    validForm = false;
+                }
+            }
+        }
+    });
+    if (validForm) {
+        // contactForm.submit();
+        contactForm.classList.add('js-validform');
+    }
+}
+
+function validationError(field, errorText) {
+    field.classList.add('js-error');
+    var p = document.createElement("p");
+    p.appendChild(document.createTextNode(errorText));
+    p.classList.add('js-error-text');
+    field.parentNode.appendChild(p);
+}
